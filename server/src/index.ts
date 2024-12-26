d20ef515e4f7adb3fd41b2993fd895bc89246bb0
Avishka <baavishka@gmail.com>
2024-12-26 14:35:23 +0530

    change env adn docker for delpoyment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-  import { Server } from "socket.io";
+import { Server } from "socket.io";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,8 +6,9 @@ import { getAllDocuments, findOrCreateDocument, updateDocument } from "./control
 
 const PORT = Number(process.env.PORT || 3000) ;
 
+// Local URL = DATABASE_URL=mongodb://127.0.0.1:27017/google-docs-clone
 /** Connect to MongoDB */
-mongoose.connect(process.env.DATABASE_URL || "", { dbName: "Google-Docs" })
+mongoose.connect(process.env.DATABASE_URL || "", { dbName: "Minimalist-Docs" })
 .then(() => { console.log("Database connected.");})
 .catch((error) => { console.log("DB connection failed. " + error);}) ;
 
@@ -38,6 +39,7 @@ io.on("connection", socket => {
       });
 
       socket.on("save-document", async (data) => {
+        
         await updateDocument(documentId, { data }) ;
       })
 
